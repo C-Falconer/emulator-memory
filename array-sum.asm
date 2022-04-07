4a1bc145aa4de2d33012b0af9b1387997c6cb798
C-Falconer <101229599+C-Falconer@users.noreply.github.com>
2022-04-07 18:37:16 -0400

    If P = NP then we would have Web Anpnps

--- array-sum.asm
+++ array-sum.asm
@@ -1,1 +1,24 @@
+; Length
+LoadDirect r0, 101
+; Index
+LoadImmediate r1, 0
+; Current Item
+LoadImmediate r2, 0
+; Scalar
+LoadDirect r3, 100
+; Offset
+LoadImmediate r4, 102
+
+; Check
+BranchOnEqual r0, r1, 13
+
+; Finagle Memory Addresses
+LoadIndirect r2, r4
+Add r2, r3
+StoreIndirect r4, r2
+AddImmediate r4, r4, 1
+AddImmediate r1, r1, 1
+BranchOnEqual r0, r1, 13
+Jump 6
+
 Halt 
